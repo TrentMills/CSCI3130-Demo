dec8b8ee618ce80f46013d44eea007e6aec41953
TrentMills <trentjmills@gmail.com>
2018-01-23 20:29:01 -0400

    CSCI 3130 - Assignment 1 - Trent Mills
    Added input text and button that updates main text.

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView tv1 = findViewById(R.id.textView1);
+        final Button btn1 = findViewById(R.id.button1);
+        final EditText txt1 = findViewById(R.id.editText1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                tv1.setText(txt1.getText().toString());
+            }
+        });
     }
 }
